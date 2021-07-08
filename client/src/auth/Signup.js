c4d2e1c5246a5fdd420cc1e5cb4d3b7c192332a4
Medina88 <Msydyka@iu.edu>
2021-07-08 17:19:45 -0400

    workout client added

---
+++ client/src/auth/Signup.js
@@ -0,0 +1,47 @@
+import React, {useState} from 'react';
+import { Form, FormGroup, Label, Input, Button } from 'reactstrap';
+
+const Signup = (props) => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [message, setMessage] = useState('')
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        if(username === ""){
+            setMessage("Username is required")
+        }
+        fetch("http://localhost:3000/user/register", {
+            method: 'POST',
+            body: JSON.stringify({user:{username: username, password: password}}),
+            headers: new Headers({
+                'Content-Type': 'application/json'
+            })
+        }).then(
+            (response) => response.json()
+        ).then ((data) => {
+            props.updateToken(data.sessionToken)
+        })
+    }
+
+
+    return(
+        <div>
+        <h1>Sign up</h1>
+        <Form onSubmit={handleSubmit}>
+            <FormGroup>
+                <Label htmlFor="username">Username</Label>
+                <Input onChange={(e) => setUsername(e.target.value)} name="username" value={username} />
+                <p>{message}</p>
+            </FormGroup>
+            <FormGroup>
+                <Label htmlFor="password">Password</Label>
+                <Input onChange={(e) => setPassword(e.target.value)} name="password" value={password} />
+            </FormGroup>
+            <Button type="submit">Sign up</Button>
+        </Form>
+    </div>
+)
+}
+
+export default Signup;
